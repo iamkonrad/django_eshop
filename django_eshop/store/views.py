6d9f6ed5e3ac1959747e080e2f77dc963edd262a
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-12 22:21:16 +0200

    Display products by category in store.html
    creation of url path in store/urls responsible for showing categories based on a slug, afterwards working on a store function in store views, the function taking a category_slug as an
    optional parameter, in case a category is associated with a slug products associated with that category will be fetched, displayed and counted, in case the slug is None(default), all available products without regard
    to their category will be fetched, counted and displayed

--- django_eshop/store/views.py
+++ django_eshop/store/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from store.models import Product
+from category.models import Category
 
 
-def store(request):
-    products = Product.objects.all().filter(is_available=True)                                                          #filtering by the availability of a product
-    product_count = products.count()                                                                                    #counting the products and assigning the nubmer to a variabe
+def store(request, category_slug=None):                                                                                 #default set to None
+    categories=None                                                                                                     #default set to None
+    products=None                                                                                                       #default set to None
+
+    if category_slug != None:                                                                                           #fetch products associated with a category
+        categories = get_object_or_404(Category,slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)                                                      #fetch all available products without filtering by category
+        product_count = products.count()                                                                                #counting the products and assigning the number to a variabe
 
     context = {
         'products':products,
