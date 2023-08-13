b1d7134acaade3aff40349d5dfe78d44751777eb
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-13 23:15:32 +0200

    CART FUNCTIONALITY 10. PROCESSING A PRODUCT ALREADY IN A CART
    Adding in_cart variable to product_detail function in store/views, it is intended to check whether a cart item matching the specified cart ID and product exist in CartItem model, it fetches the cart(based on session ID), filters the cart to include only the ones associated with the specified product returning
    True if a cart item exists and False, if it does not, afterwards passing in_cart into a context dictionary; in product_detail.html, if a product has aready been added to a cart it will not display add to cart button anymore, adding an if statement in product_detail.html that if a product is already in_cart a "Added to cart" button will appear instead of "Add to cart", else
    (if a product is not in_cart yet) a standard Add to cart button will appear; Inside an if statement in_cart statement adding a View Cart button for viewing an item already in cart next to Added to cart button (will only become visible if a product is already in cart and will redirect to cart url)

--- django_eshop/store/views.py
+++ django_eshop/store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+
+from carts.models import CartItem
 from store.models import Product
 from category.models import Category
+from carts.views import _cart_id
 
 
 def store(request, category_slug=None):                                                                                 #default set to None
@@ -22,13 +25,14 @@ def store(request, category_slug=None):                                         
     return render (request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
-    try:
-        single_product= Product.objects.get(category__slug=category_slug, slug=product_slug)                            #accessing Product model's category and Category model's
-                                                                                                                        #slug; retrieving a product equal to given category slug and
-    except Exception as e:                                                                                              #product slug
+    try:                                                                                                                #accessing Product model's category and Category model's slug;
+        single_product= Product.objects.get(category__slug=category_slug, slug=product_slug)                            #retrieving a product equal to given category slug and product slug;
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()             #filtering products, returning True or False, based on whether they exist in a cart;
+    except Exception as e:
         raise e
 
     context={
         'single_product':single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'store/product_detail.html', context)
