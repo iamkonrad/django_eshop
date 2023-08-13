ebf228dc790eb309f455cc1995aed69708899ff0
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-14 01:40:19 +0200

    PAGINATION PART 1. LOGIC
    Inside store apps views importing Paginator; adding 3 variables to if statement of store function responsible for displaying products based on a specific category (paginator- responsible for the number of items displayed per page-passing the page number using get method, page and paged_products- storing page inside a variable, which gets passed inside a context dictionary as a value
    linked to a key product), afterwards adding the same variables to else part of the statement responsible for fetching all products;

--- django_eshop/store/views.py
+++ django_eshop/store/views.py
@@ -4,6 +4,7 @@ from carts.models import CartItem
 from store.models import Product
 from category.models import Category
 from carts.views import _cart_id
+from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
 
 
 def store(request, category_slug=None):                                                                                 #default set to None
@@ -13,14 +14,21 @@ def store(request, category_slug=None):                                         
     if category_slug != None:                                                                                           #fetch products associated with a category
         categories = get_object_or_404(Category,slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 9)                                                                              # number of products displayed per page
+        page = request.GET.get('page')                                                                                  # passing page number through get method
+        paged_products = paginator.get_page(page)
         product_count = products.count()
-    else:
-        products = Product.objects.all().filter(is_available=True)                                                      #fetch all available products without filtering by category
+    else:                                                                                                               #fetch all available products without filtering by category
+        products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 9)                                                                              # number of products displayed per page
+        page = request.GET.get('page')                                                                                  # passing page number through get method
+        paged_products = paginator.get_page(page)
         product_count = products.count()                                                                                #counting the products and assigning the number to a variabe
 
     context = {
-        'products':products,
+        'products':paged_products,                                                                                      # paged 9 products
         'product_count':product_count,
+
     }
     return render (request, 'store/store.html', context)
 
