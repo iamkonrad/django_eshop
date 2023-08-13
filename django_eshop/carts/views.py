a11c5cc1a6319e53ff42ff489502fa3a1881d3df
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-13 07:15:48 +0200

    CART FUNCTIONALITY 3. ADD TO CART(GUEST USER);
    Creation of add_cart function in carts/views, it operates based on session_keys, in case there is an existing cart if fetches the items, in case there is no cart new one is created,  afterwards finding existing cart item for the specific product and cart, if the
    item exists in the cart its quantity gets incremented by one, in case it does not it gets created (with default quantity set to 1) and linked to current product and cart, at the end the user gets redirected to cart url seeing the updated cart; creation of path for the add_cart function - add_cart url in carts urls;
    adding the url in product_detail.html template  linkingadd cart button to add_cart url and passing the single_product.id as an argument to add_cart view, now clicking on add cart button will add 1 quantity of a specific product to the cart;

--- django_eshop/carts/views.py
+++ django_eshop/carts/views.py
@@ -1,4 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from carts.models import Cart, CartItem
+from store.models import Product
+
+
+def _cart_id(request):                                                                                                  #private function
+    cart = request.session.session_key                                                                                  #session id
+    if not cart:
+        cart = request.session.create()                                                                                 #if there is no session_key a new one is created
+    return cart
+
+def add_cart (request, product_id):
+    product = Product.objects.get(id=product_id)                                                                        #fetching the product
+    try:
+        cart=Cart.objects.get(cart_id=_cart_id(request))                                                                #retrieving the cart based on the present session id
+    except Cart.DoesNotExist:                                                                                           # if no cart exists a new one is created using the present session id
+        cart = Cart.objects.create(
+            cart_id=_cart_id(request)
+        )
+    cart.save()
+
+    try:                                                                                                                #finding existing cart item for the specific product and cart
+        cart_item = CartItem.objects.get(product=product,cart=cart)
+        cart_item.quantity +=1                                                                                          #if the item exists in the cart, quantity incremented by one
+        cart_item.save()
+    except CartItem.DoesNotExist:                                                                                       #creating a new cart item if there is none(quantity=1); linked
+        cart_item = CartItem.objects.create(                                                                            #to current product and cart
+            product=product,
+            quantity = 1,
+            cart=cart,
+        )
+        cart_item.save()
+    return redirect('cart')                                                                                             #redirection, updated cart
 
 def cart(request):
     return render(request, 'store/cart.html')
