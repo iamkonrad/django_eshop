d9a111e3b4a1ef2d389add9e398c211325d6d1a0
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-15 06:42:22 +0200

    REGISTRATION 2. USER CREATION/REGISTRATION FORM
    In accounts views registration function, working on user creation, if a request is POST then potential user should receive a user creation form (based on MyAccountManager model), with
    username based on email (the user doesn't get to choose a username), in case it's GET a user registration form will still show up; in forms.py creation of init function that will together with a for loop set equally looking placeholders
    for all the html elements ('form-control') inheriting from this function; creation of a clean function in forms.py that will raise an error if confirmed password and pasword aren't the same, passing reference to form.emailerrors and
    form.non_field_errors into register.html template, so scenarios when a prospective user tries to use an already used email address and types two wrong passwords are covered

--- django_eshop/accounts/views.py
+++ django_eshop/accounts/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from accounts.forms import RegistrationForm
+from accounts.models import Account
 
 
 def register(request):
-    form = RegistrationForm()
+    if request.method =='POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name= form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username= email.split("@")[0]
+
+            user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,
+                                               password=password)                                                       #from django MyAccManager model, passing all the above
+            user.phone_number = phone_number                                                                            #can't be used in user directly bcz of model logic
+            user.save()
+        else:
+            form = RegistrationForm()
     context = {
         'form':form,
     }
