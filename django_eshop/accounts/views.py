a7f636ef83c970a94fe10e416f6fedf5fbcb62fd
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-16 03:16:29 +0200

    FORGOT PASSWORD FUNCTIONALITY
    Creation of forgot_password function in accounts view and its corresponding url pattern , if the request method is POST the function will first try to extract the email address
    from the form, then check if there is a user account associated with that email and retrieve the user account if it exists, then create and send password retrieval email (same fields as in registration form,
    with the exception of reset_password_email.html, which will be created on this occasion), afterwards a user will be notfied that a password reset email has been sent to his account and he will be redirected to login page,
    in case there is no account associated with that particular email address a guest  user will be redirected to forgot_password; creation and styling of forgot_password.html in templates/accounts; creation of
    resetpassword_validate function in accounts views and its corresponding url;creation and styling of reset_password_email.html (in the templates/accounts), adding a url reference to reset password_validate to
    reset_password_email

--- django_eshop/accounts/views.py
+++ django_eshop/accounts/views.py
@@ -14,6 +14,7 @@ from django.core.mail import EmailMessage
 
 
 def register(request):
+
     if request.method =='POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -44,6 +45,9 @@ def register(request):
             return redirect('/accounts/login/?command=verification&email='+email)
         else:
             form = RegistrationForm()
+    else:
+        form=RegistrationForm()
+
     context = {
         'form':form,
     }
@@ -91,3 +95,33 @@ def activate(request, uidb64, token):
 def dashboard(request):
     return render(request,'accounts/dashboard.html')
 
+def forgot_password(request):
+    if request.method =='POST':
+        email=request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user=Account.objects.get(email__exact=email)
+
+        # RESET PASSWORD EMAIL
+            current_site = get_current_site(request)
+            mail_subject = "Password reset"
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+
+        else:
+            messages.error(request, 'No such account exists!')
+            return redirect('forgot_password')
+
+    return render(request, 'accounts/forgot_password.html')
+
+def resetpassword_validate(request):
+    return
