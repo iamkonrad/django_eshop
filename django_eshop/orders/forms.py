6d1ae536a0b3c6a0de829feb71d28b74115e1466
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-17 05:34:10 +0200

    CHECKOUT: PAYMENT PAGE
    In carts app creation of payments function and its corresponding url (rendering payments.html); creation of payments.html template inside orders/templates, styling of payments.html page;
    in orders app views place_order function inside if POST creating an order variable, which retrieves from the database an object with a specific order number belonging to the user (not ordered), afterwards it
    constructs a context dictionary with pairs holding data about order, cart items, total, tax and grand total), the context is then passed into payments.html; making changes in orders models Order model concancetating
    the display of full name and full_address so that they display together, afterwards, passing references to full_name and full_address into payments.html (Billing Details), also passing references to the fields based
    on the order model such as:city (order.city),state (order.state),country(order.country), email (order.email) and phone (order.phone) into the template, also adding an if statement into payments.html that will display
    the order note if it was added to the order (an optional field), styling payment methods and review products fields, inside review products adding references to  cartitem model, products inside the cart will now get
    dynamically displayed here

--- django_eshop/orders/forms.py
+++ django_eshop/orders/forms.py
@@ -4,4 +4,4 @@ from .models import Order
 class OrderForm(forms.ModelForm):
     class Meta:
         model=Order
-        fields=['first_name','last_name','phone','email,address_1','address_2','country','state','city','order_note']
+        fields=['first_name','last_name','phone','email','address_1','address_2','country','state','city','order_note']
--- django_eshop/orders/views.py
+++ django_eshop/orders/views.py
@@ -7,6 +7,8 @@ from .models import Order
 import datetime
 
 
+def payments(request):
+    return render(request,'orders/payments.html')
 def place_order(request, total=0,quantity=0):
     current_user = request.user                                                                                         #logged in user
 
@@ -54,7 +56,17 @@ def place_order(request, total=0,quantity=0):
             order_number=current_date + str(data.id)
             data.order_number=order_number
             data.save()
-            return redirect('checkout')
+
+            order=Order.objects.get(user=current_user,is_ordered=False, order_number=order_number)
+            context= {
+                'order':order,
+                'cart_items': cart_items,
+                'total': total,
+                'tax':tax,
+                'grand_total':grand_total,
+            }
+
+            return render(request,'orders/payments.html',context)
     else:
         return redirect ('checkout')
 
