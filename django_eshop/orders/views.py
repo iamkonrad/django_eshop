7cf0c1db0c230d0dc6bdcf05e167145d299960ad
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-17 03:55:07 +0200

    PLACE AN ORDER: VIEWS + UNIQUE NUMBER
    Inside orders views working on a place_order function, the function fetches the logged-in user and cart items associated with him, checks the cart and in case it's empty
    redirects to the store, based on the cart calculates the total and tax, if statement checks whether a sent request was a POST request: in case it was it validates the order form, creates new order and populates it,
    then it generates the order number (unique) and saves it, afterwards the user gets redirected to checkout page, in case the request was not sent via POST the user simply gets redirected to checkout.html;
    adding a reference to place_order url inside checkout.html

--- django_eshop/orders/views.py
+++ django_eshop/orders/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 
 from carts.models import CartItem
+from orders.forms import OrderForm
+from. forms import OrderForm
+from .models import Order
+import datetime
 
 
-def place_order(request):
+def place_order(request, total=0,quantity=0):
     current_user = request.user                                                                                         #logged in user
 
 
@@ -12,5 +16,48 @@ def place_order(request):
     if cart_count <=0:
         return redirect('store')
 
+    grand_total=0
+    tax=0
+
+    for cart_item in cart_items:
+        total += (cart_item.product.price * cart_item.quantity)                                                         # subtotal, product price by its quantity, adding to the total
+        quantity += cart_item.quantity                                                                                  # adding the quantity of every cart item to the overall one
+    tax = (16 * total) / 100
+    grand_total = total + tax
+
     if request.method=='POST':
         form=OrderForm(request.POST)
+        if form.is_valid():
+            data=Order()
+            data.user = current_user
+            data.first_name=form.cleaned_data['first_name']
+            data.last_name=form.cleaned_data['last_name']
+            data.phone=form.cleaned_data['phone']
+            data.email=form.cleaned_data['email']
+            data.address_1=form.cleaned_data['address_1']
+            data.address_2 = form.cleaned_data['address_2']
+            data.country = form.cleaned_data['country']
+            data.state = form.cleaned_data['state']
+            data.city = form.cleaned_data['city']
+            data.order_note = form.cleaned_data['order_note']
+            data.order_total= grand_total
+            data.tax= tax
+            data.ip=request.META.get('REMOTE_ADDR')
+            data.save()
+
+            #Order number
+            yr=int(datetime.date.today().strftime('%Y'))
+            day=int(datetime.date.today().strftime('%d'))
+            mt=int(datetime.date.today().strftime('%m'))
+            d=datetime.date(yr,mt,day)
+            current_date=d.strftime("%Y%mt%d")
+            order_number=current_date + str(data.id)
+            data.order_number=order_number
+            data.save()
+            return redirect('checkout')
+    else:
+        return redirect ('checkout')
+
+
+
+
