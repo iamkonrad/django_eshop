96303529f282893397674a0256664be1c3cf2694
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-17 02:39:13 +0200

    PLACE AN ORDER: URLS,VIEWS,FORM
    Registering orders apps urls in django_eshop urls; in orders views and urls creation of a place_orders function and its corresponding url path, using if statement, after clicking
    on place an order the user will be redirected back to store page if his cart count is lower or equal than 0, additionon of another if statement that will check whether an orderform was submitted via POST request;
    creation of forms.py folder in orders app and working on OrderForm fields therein based on a Order model

--- django_eshop/orders/views.py
+++ django_eshop/orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from carts.models import CartItem
+
+
+def place_order(request):
+    current_user = request.user                                                                                         #logged in user
+
+
+    cart_items=CartItem.objects.filter(user=current_user)                                                               #if cart count less or equal to 0 redirect back to shop
+    cart_count=cart_items.count()
+    if cart_count <=0:
+        return redirect('store')
+
+    if request.method=='POST':
+        form=OrderForm(request.POST)
