95de933d5a2409f0d95b1112e06d57cf5699c849
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-18 16:57:06 +0200

    REVIEWS AND RATINGS: 8 DEBUGGING THE REVIEW STARS
    Fixed an issue where stars inside of product_detail.html were displaying, but were not being styled by the custom.css, the issue was caused by
    pycharm by default adding the path to custom.css inside root/static css folder, moving the file to main app's static folder has solved the problem; removing the spaces between font-awesome icons and
    the if statements in product_detail.html removed the problem of review stars not getting updated based on the previous rating, now the rating system works as intended

--- django_eshop/django_eshop/category/context_processor.py
+++
@@ -1,6 +0,0 @@
-from category.models import Category
-
-
-def menu_links(request):
-    links = Category.objects.all()                                                                                      #fetching all the categories
-    return dict(links=links)                                                                                            #returning a dictionary with key links connected to category queryset
--- django_eshop/django_eshop/orders/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .import views
-
-
-urlpatterns = [
-    path('place_order/',views.place_order,name='place_order'),
-    path('payments/',views.payments,name='payments'),
-]
-
