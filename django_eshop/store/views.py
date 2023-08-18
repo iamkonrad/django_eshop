7addadcbaf7629c5dabb8e9b47587028c8634d97
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-18 09:47:18 +0200

    REVIEWS AND RATINGS 4: TYING REVIEWS TO PURCHASES
    Only users who bought a particular product will be allowed to review it; inside store app's product_detail function adding a try and except block
    that will check whether a particular product exists for a particular user and return it into the context dictionary, in case it doesn't it will return None; inside product_detail.html adding another if
    statement containing value of order product return from product_detail function, if a user has not purchased a particular product before, he will be shown a message that he needs to buy it first in order to be
    able to review it, the submit button won't show for him as well

--- django_eshop/store/views.py
+++ django_eshop/store/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import CartItem
+from orders.models import OrderProduct
 from store.forms import ReviewForm
 from store.models import Product, ReviewRating
 from category.models import Category
@@ -42,9 +43,16 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    try:
+        orderproduct=OrderProduct.objects.filter(user=request.user,product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct=None
+
+
     context={
         'single_product':single_product,
         'in_cart': in_cart,
+        'orderproduct':orderproduct,
     }
     return render(request, 'store/product_detail.html', context)
 
@@ -85,6 +93,3 @@ def submit_review(request, product_id):
                 data.save()
                 messages.success(request, 'Thank. Your review has been submitted.')
                 return redirect(url)
-
-
-
