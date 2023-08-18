40328904e9599a3233461238c7da4c545648713d
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-18 09:24:14 +0200

    REVIEWS AND RATINGS: 3 LOGIC AND DISPLAY
    1: The display, Inside custom.css hiding the radio buttons and showing the stars instead by styling the element using FontAwesome, inserting Font Awesome reference
    into base.html,so that it displays properly; styling stars in custom.css;2:The logic for submitting and saving reviews, inside store app views creation of submit_review function and its corresponding url path,
    inserting the submit_review url reference into product_display.html; submit_review function function first checks if the submit method is POST, afterwards adding a try and except block, if there is already
    a product review submitted by a user it will get updated and user will be shown a success message, if there is none and the form is valid then a new "instance" review will be created; adding an if statement
    inside product_display_review.html if a user is logged in then he will be able to submit a review, otherwise he will be forced to log in in order to add a review

--- django_eshop/store/views.py
+++ django_eshop/store/views.py
@@ -1,11 +1,13 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import CartItem
-from store.models import Product
+from store.forms import ReviewForm
+from store.models import Product, ReviewRating
 from category.models import Category
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger, Paginator
+from django.contrib import messages
 
 
 def store(request, category_slug=None):                                                                                 #default set to None
@@ -51,7 +53,7 @@ def search(request):
     if 'keyword' in request.GET:                                                                                        # extracting the search keyword value
         keyword = request.GET['keyword']                                                                                #storing it inside the keyword variable
         if keyword:
-            products = Product.objects.order_by('created_date').filter(Q(desctiption__icontains=keyword)|                #looking for a product reference in description and product_name
+            products = Product.objects.order_by('created_date').filter(Q(description__icontains=keyword)|                #looking for a product reference in description and product_name
                                                                        Q(product_name__icontains=keyword))               #by keyword
             product_count = products.count()
 
@@ -60,3 +62,29 @@ def search(request):
         'product_count': product_count,
     }
     return render(request,'store/store.html',context)
+
+def submit_review(request, product_id):
+    url=request.META.get('HTTP_REFERER')
+    if request.method=='POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id,product__id=product_id)
+            form = ReviewForm(request.POST,instance=reviews)
+            form.save()
+            messages.success(request,'Thank. Your review has been updated.')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data=ReviewRating()
+                data.subject=form.cleaned_data['subject']
+                data.rating=form.cleaned_data['rating']
+                data.review=form.cleaned_data['review']
+                data.ip=request.META.get('REMOTE_ADDR')
+                data.product_id=product_id
+                data.user_id=request.user.id
+                data.save()
+                messages.success(request, 'Thank. Your review has been submitted.')
+                return redirect(url)
+
+
+
