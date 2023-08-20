f87e23c8aec98fdba52d2d2bcfcb6867873f53e6
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-20 08:54:00 +0200

    PRODUCT RATINGS ON HOMEPAGE
    Inside main app's views home function adding products variable that will retrieve all the available products and order them by creation data, afterwards a for loop will loop
    through the products and retrieve each product's reviews, at the end both products and reviews will be passed to a context dictionary and from there to home.html; inside home.html adding if statements for
    product.averageReview div elements, so that all the products showing on the main page of the store will now feature their average ratings (the number of rating stars)

--- django_eshop/django_eshop/views.py
+++ django_eshop/django_eshop/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, ReviewRating
+
 
 def home(request):
-    products = Product.objects.all().filter(is_available=True)                                                          #filtering by the availability of a product
+    products = Product.objects.all().filter(is_available=True).order_by('created_date')                                 #filtering by the availability of a product
+
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id,status=True)                                        #Get the reviews
+
 
     context = {
         'products':products,
+        'reviews':reviews,
     }
 
     return render (request, 'home.html',context)
